0100a9625b543d70fdd63e9ed0bbaec6d8ac57fe
Hartmann <8162110@souclaretiano.edu.br>
2023-08-21 21:28:05 -0400

    commit inicial

---
+++ MeuProjeto.java
@@ -0,0 +1,10 @@
+// primeiro arquivo
+public class MeuProjeto {
+    public static void main(String[] args){
+        int idade = 36;
+        double peso = 87.5;
+        char letra = 'a';
+        String texto = "Valores: ";
+        System.out.println(texto + idade);
+    }
+}
